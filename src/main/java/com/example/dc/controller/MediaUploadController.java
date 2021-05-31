964960f4d21ab7780b102ed54a1018b5a723c30b
dell <1634450780@qq.com>
2021-05-31 19:34:34 +0800

    新增MediaController接口

---
+++ src/main/java/com/example/dc/controller/MediaUploadController.java
@@ -0,0 +1,50 @@
+package com.example.dc.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.example.dc.common.response.ResponseResult;
+import com.example.dc.pojo.media.response.CheckChunkResult;
+import com.example.dc.service.MediaUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/media/upload")
+public class MediaUploadController {
+    @Autowired
+    MediaUploadService mediaUploadService;
+
+    @PostMapping("/register")
+    public ResponseResult register(String identifier, String filename, Long totalSize) {
+        return mediaUploadService.register(identifier,filename,totalSize);
+    }
+
+
+    @PostMapping("/checkchunk")
+    public CheckChunkResult checkchunk(String fileMd5, Integer chunk, Integer chunkSize) {
+        return mediaUploadService.checkchunk(fileMd5,chunk,chunkSize);
+    }
+
+
+    @PostMapping("/uploadchunk")
+    public ResponseResult uploadchunk(MultipartFile file, Integer chunkNumber, String identifier) {
+        return mediaUploadService.uploadchunk(file,identifier,chunkNumber);
+    }
+    @GetMapping("/uploadchunk")
+    public ResponseResult uploadchunk(@RequestParam("chunkNumber") Integer chunkNumber, @RequestParam("identifier") String identifier, @RequestParam("filename") String filename) {
+
+        return mediaUploadService.register2(identifier,chunkNumber,filename);
+    }
+
+
+    @PostMapping("/mergechunks")
+    public ResponseResult mergechunks(@RequestBody String request) {
+        JSONObject jsonObject = JSON.parseObject(request);
+        Long fileSize = jsonObject.getLong("fileSize");
+        String fileMd5 = jsonObject.getString("fileMd5");
+        String fileName = jsonObject.getString("fileName");
+        return mediaUploadService.mergechunks(fileMd5,fileName,fileSize);
+    }
+}
